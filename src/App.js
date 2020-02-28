7ae1ca6f96b47ad2dc5ef4cb17fcbdacf2653846
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-28 11:30:58 -0500

    made the routes only home works and gonna move on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import { Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
+      <Route path="/characterlist">
+        <CharacterList />
+      </Route>
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/characterlist">Characters</Link>
+      </Router>
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
     </header>
   );
